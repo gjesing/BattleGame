03e452d9fe1325c1fb4f8a4977cc090926ad048a
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-02-27 10:38:38 +0100

    Add project files.

---
+++ BattleGame.Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleGame.Library
+{
+    public class Class1
+    {
+    }
+}
